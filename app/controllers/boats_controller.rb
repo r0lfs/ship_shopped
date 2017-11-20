36e576e6cef1e40d7eface5a52cdf263297b4aa1
r0lfs <rdwoolen@gmail.com>
2017-11-20 15:29:16 -0700

    have the databases set up and adding new boat functionality

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,11 +1,19 @@
 class BoatsController < ApplicationController
   def index
+    @boats = Boat.all
   end
 
   def new
+    @boat = Boat.new
   end
 
   def create
+    @boat = Boat.new(boat_params)
+    if @boat.save
+      redirect_to root_path      
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -18,5 +26,11 @@ class BoatsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:user_id, :location, :containers, :ship_name)
   end
 end
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,2 +1,7 @@
 class Boat < ApplicationRecord
+	belongs_to :user
+	has_many :jobs, through: :contracts
+
+	validates :ship_name, presence: true, uniqueness: true
+	validates :containers, numericality: { only_integer: true }
 end
