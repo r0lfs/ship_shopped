76c0ce61abba666552ae55bb6d13ed74463ca1c4
r0lfs <rdwoolen@gmail.com>
2017-11-27 22:12:05 -0700

    have auto execute for jobs working after fulfilment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127220848) do
+ActiveRecord::Schema.define(version: 20171128035849) do
 
   create_table "boats", force: :cascade do |t|
     t.integer "user_id"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20171127220848) do
     t.string "job_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "open", default: true
+    t.boolean "complete", default: false
     t.index ["job_name"], name: "index_jobs_on_job_name", unique: true
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
